7c301bd7457b8eec820253477e015179fbc1420d
Maju Testoni <majuliatestoni@gmail.com>
2022-08-09 14:20:24 -0300

    projeto pronto mas sem html e css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { CepServiceService } from './cep-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cep';
+
+  constructor(private cepService: CepServiceService) {}
+
+  consultaCep(valor, form) {
+    this.cepService.buscar(valor).subscribe((dados) => {
+      this.populaForm(dados, form);
+    });
+  }
+
+  populaForm(dados, form) {
+    form.setValue({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      uf: dados.uf,
+    });
+  }
 }
